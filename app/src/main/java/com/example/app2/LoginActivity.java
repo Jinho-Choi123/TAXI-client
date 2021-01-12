62b33c845125be224724223614f302b1aa1f5329
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-12 15:26:12 +0900

    feature:Add check permission, allowing permission dialog

--- app/src/main/java/com/example/app2/LoginActivity.java
+++ app/src/main/java/com/example/app2/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,10 +36,34 @@ import static android.widget.Toast.LENGTH_SHORT;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private void checkPermission() {
+        if (
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        )
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+            ) {
+
+                //사용자가 허용
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        0);
+
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        checkPermission();
 
         Button login_btn = (Button) findViewById(R.id.loginBtn);
         EditText userId = (EditText) findViewById(R.id.userId);
@@ -44,6 +72,8 @@ public class LoginActivity extends AppCompatActivity {
         String url = "http://192.249.18.169:8080/auth/login";
 
         Context context = LoginActivity.this;
+
+
         login_btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
